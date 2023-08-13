149b31ffc929e7ed7dbdb953c4eb0410bd2de0f6
Ekaterina <120379585+Ekkaterinka@users.noreply.github.com>
2023-08-13 16:47:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React_redux_toolkit_saga_online_shoe_store/',
   plugins: [react()],
 })
